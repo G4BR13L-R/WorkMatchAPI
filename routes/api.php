4bbf37aca686e3601783ab49f6c8190e207b9326
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-08-05 22:11:41 -0400

    refactor: Reorganizado as rotas e adicionado rotas protegidas conforme o tipo de usuário

--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,28 @@ use App\Http\Controllers\Auth\ContratadoAuthController;
 use App\Http\Controllers\Auth\ContratanteAuthController;
 use Illuminate\Support\Facades\Route;
 
+// Rotas publicas
 Route::prefix('contratante')->controller(ContratanteAuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('sessions', 'login');
-
-    Route::middleware(['auth:sanctum'])->group(function () {
-        Route::delete('sessions', 'logout');
-    });
 });
 
 Route::prefix('contratado')->controller(ContratadoAuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('sessions', 'login');
+});
 
-    Route::middleware(['auth:sanctum'])->group(function () {
-        Route::delete('sessions', 'logout');
+// Rotas protegidas
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::delete('contratante/sessions', [ContratanteAuthController::class, 'logout']);
+    Route::delete('contratado/sessions', [ContratadoAuthController::class, 'logout']);
+
+    Route::prefix('contratante')->middleware('check.type:contratante')->group(function () {
+        // Rotas protegidas para contratantes
+    });
+
+    Route::prefix('contratado')->middleware('check.type:contratado')->group(function () {
+        // Rotas protegidas para contratados
     });
 });
