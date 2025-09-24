4397c0d693661aadc3ae4ba0bb85a8dad7d577f5
Gabriel Silva de Rezende <gabrielrezende23@hotmail.com>
2025-09-24 00:28:32 -0400

    refactor: Ajustado metodo toArray

--- app/Models/Oferta.php
+++ app/Models/Oferta.php
@@ -51,4 +51,17 @@ class Oferta extends Model
     {
         return $this->hasMany(Avaliacao::class);
     }
+
+    public function toArray()
+    {
+        $data = parent::toArray();
+
+        $data = array_merge($data, [
+            'endereco' => $this->endereco ? $this->endereco->toArray() : null,
+            'contratante' => $this->contratante ? $this->contratante->toArray() : null,
+            'contratado' => $this->contratado ? $this->contratado->toArray() : null,
+        ]);
+
+        return $data;
+    }
 }
